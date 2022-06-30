3a107c2494ca9fe7bd418f38ab1a6ec1afa61141
Taratora117 <alex.j.hkrystev@gmail.com>
2022-06-30 10:38:32 +0300

    added cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-inm+&d&x!d8*q&*30*u+(!e8b2cnj&n=ze4$-ud)c1!n$u%1_l
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -36,13 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
